8400c80ac29d133f5c2fe104db45787a98d78ea1
Dominik Bauchner <dbauchner@gmail.com>
2022-03-05 16:23:15 +0100

    first commit

--- CPP/prvni_prednaska_2/main.cpp
+++ CPP/prvni_prednaska_2/main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 using namespace std;
 
-class Student:{
-private:
+class Student{
+public:
     string jmeno;
     string rodneCislo;
     int semestr;
 };
 
 int main() {
+    Student* studentDominik = new Student();
+    studentDominik-> jmeno = "Dominik";
+    studentDominik->rodneCislo = "123445/8745";
+    studentDominik->semestr = 2;
+    cout << "Jmeno studenta je: " << studentDominik->jmeno << "\n";
+    cout << "Semestr studenta: " << studentDominik->semestr << endl;
 
-    cout << "Hello, World!" << endl;
+    delete studentDominik;
     return 0;
 }
