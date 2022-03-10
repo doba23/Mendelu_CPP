51ca1503214168b0ae2f97f716c3b144181d5645
Dominik Bauchner <dbauchner@gmail.com>
2022-03-10 10:55:52 +0100

    commit

--- CPP/druha_prednaska_1/main.cpp
+++ CPP/druha_prednaska_1/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 using namespace std;
 
+class Postava {
+public:
+    float m_zdravi;
+    int m_pocetZlataku;
+    int m_vahaNakladu;
+
+    void setzdravi(float zdravi){
+    m_zdravi = zdravi;
+    }
+    float getZdravi(){
+        return m_zdravi;
+    }
+    float getPocetZlataku(){
+        return m_pocetZlataku;
+    }
+
+    Postava(int pocetZlataku){
+        m_zdravi = 100.0f;
+        m_pocetZlataku = pocetZlataku;
+        m_vahaNakladu = 0;
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Postava* frodo = new Postava (0);
+    frodo->setzdravi(60.0);
+    cout << "Zdravi Froda: " << frodo->getZdravi() << endl;
+    cout << "Pocet zlataku: " << frodo->getPocetZlataku() << endl;
+    delete frodo;
     return 0;
 }
