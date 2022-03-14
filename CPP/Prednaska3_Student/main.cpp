bf7eac48b9ba6360678d0afefb7d8d573f64aca3
Dominik Bauchner <dbauchner@gmail.com>
2022-03-14 22:28:54 +0100

    commit

--- CPP/Prednaska3_Student/main.cpp
+++ CPP/Prednaska3_Student/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+using namespace std;
 
-class Student(){
+class Student{
     string m_jmeno;
     int m_semestr;
     float m_prumer;
@@ -12,22 +13,24 @@ public:
         m_prumer = prumer;
         m_soucetStipendii = 0.0;
 }
-
     float aktualniStipendium(){
-        return ((1000*m_prumer)+(m_semestr*100));
+        float aktualniVyse= ((1000*m_prumer)+(m_semestr*100));
+        return aktualniVyse;
     }
-    float getSoucetStipendii(){
-        return m_soucetStipendii;
+
+    void printSoucetStipendii(){
+        cout << "Vyplaceno: " << m_soucetStipendii << endl;
+        cout << "Aktualni vyse stipendia: " << aktualniStipendium()  << endl;
     }
-    void setAddSoucetStipedndii(){
-        m_soucetStipendii += aktualniStipendium;
+
+    void setAddSoucetStipedndii() {
+        m_soucetStipendii += aktualniStipendium();
     }
-    int getAktualniStipendium(){
-        return aktualniStipendium();
-    }
+
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Student* dominik = new Student("Dominik", 2, 3.5);
+    dominik->printSoucetStipendii();
     return 0;
 }
