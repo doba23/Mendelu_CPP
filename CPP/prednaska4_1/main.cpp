8310cc93a7ed1bc297eddb932c4adcd0dcf42ab4
Dominik Bauchner <dbauchner@gmail.com>
2022-03-16 22:05:12 +0100

    commit

--- CPP/prednaska4_1/main.cpp
+++ CPP/prednaska4_1/main.cpp
@@ -2,23 +2,45 @@
 using namespace std;
 
 class Pocitadlo{
-public:
+private:
     static int s_pocitadlo;
-
+public:
     Pocitadlo(){
         s_pocitadlo++;
     }
-
     ~Pocitadlo(){
         s_pocitadlo--;
     }
+    static int getPocetInstanci(){
+        return s_pocitadlo;
+    }
+
 };
 
 int Pocitadlo::s_pocitadlo = 0;
 
 int main() {
+
+    cout << "Pocet instanci: " << Pocitadlo::getPocetInstanci() << endl;
+
+//    cout << "Pocet instanci: " << Pocitadlo::s_pocitadlo << endl;
+
     Pocitadlo* jedna = new Pocitadlo();
-    cout << "Pocet instanci: " << jedna->s_pocitadlo << endl;
+//    cout << "Pocet instanci: " << jedna->s_pocitadlo << endl;
+    cout << "Pocet instanci: " << Pocitadlo::getPocetInstanci() << endl;
+
+
+    Pocitadlo* dva = new Pocitadlo();
+//    cout << "Pocet instanci: " << jedna->s_pocitadlo << endl;
+    cout << "Pocet instanci: " << Pocitadlo::getPocetInstanci() << endl;
+
+
+    Pocitadlo* tri = new Pocitadlo();
+//    cout << "Pocet instanci: " << jedna->s_pocitadlo << endl;
+    cout << "Pocet instanci: " << Pocitadlo::getPocetInstanci() << endl;
+
     delete jedna;
+    delete dva;
+    delete tri;
     return 0;
 }
