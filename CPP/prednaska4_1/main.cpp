f5927e7a81a9d6ff2373f515005357e370cb9ba6
Dominik Bauchner <dbauchner@gmail.com>
2022-03-24 06:10:28 +0100

    commit

--- CPP/prednaska4_1/main.cpp
+++ CPP/prednaska4_1/main.cpp
@@ -2,45 +2,33 @@
 using namespace std;
 
 class Pocitadlo{
-private:
     static int s_pocitadlo;
+    string m_jmeno;
 public:
     Pocitadlo(){
-        s_pocitadlo++;
+        s_pocitadlo ++;
+        m_jmeno = "Vychozi jmeno"
     }
     ~Pocitadlo(){
-        s_pocitadlo--;
+        s_pocitadlo --;
     }
     static int getPocetInstanci(){
         return s_pocitadlo;
     }
-
 };
-
-int Pocitadlo::s_pocitadlo = 0;
+    int Pocitadlo::s_pocitadlo = 0;
 
 int main() {
-
     cout << "Pocet instanci: " << Pocitadlo::getPocetInstanci() << endl;
-
-//    cout << "Pocet instanci: " << Pocitadlo::s_pocitadlo << endl;
-
     Pocitadlo* jedna = new Pocitadlo();
-//    cout << "Pocet instanci: " << jedna->s_pocitadlo << endl;
     cout << "Pocet instanci: " << Pocitadlo::getPocetInstanci() << endl;
-
-
     Pocitadlo* dva = new Pocitadlo();
-//    cout << "Pocet instanci: " << jedna->s_pocitadlo << endl;
     cout << "Pocet instanci: " << Pocitadlo::getPocetInstanci() << endl;
-
-
-    Pocitadlo* tri = new Pocitadlo();
-//    cout << "Pocet instanci: " << jedna->s_pocitadlo << endl;
-    cout << "Pocet instanci: " << Pocitadlo::getPocetInstanci() << endl;
-
-    delete jedna;
     delete dva;
+    cout << "Pocet instanci: " << Pocitadlo::getPocetInstanci() << endl;
+    Pocitadlo* tri = new Pocitadlo();
+    cout << "Pocet instanci: " << Pocitadlo::getPocetInstanci() << endl;
+    delete jedna;
     delete tri;
     return 0;
 }
--- CPP/prednaska4_2/main.cpp
+++ CPP/prednaska4_2/main.cpp
@@ -3,25 +3,32 @@ using namespace std;
 
 class Game{
 private:
-    static string s_jmenoHrace;
+    static string s_jmeno;
     static int s_pocetBodu;
 public:
-    static string getPocetBodu(){
+    static int getPocetBodu(){
         return s_pocetBodu;
     }
     static string getJmenoHrace(){
-        return s_jmenoHrace;
+        return s_jmeno;
     }
-    void setBody(int pocetBodu){ // set=nastav
+    static void setBody(int pocetBodu){
         s_pocetBodu = pocetBodu;
     }
+    static void setJmeno(string jmeno){
+        s_jmeno = jmeno;
+    }
+
 };
 
-
-string Game::s_jmenoHrace = "zadej jmeno";
 int Game::s_pocetBodu = 0;
+string Game::s_jmeno = "zadej jmeno";
 
 int main() {
-
+    Game* prvni = new Game;
+    prvni->setBody(4);
+    prvni->setJmeno("Dominik");
+    cout << Game::getPocetBodu() << endl;
+    cout << Game::getJmenoHrace() << endl;
     return 0;
 }
