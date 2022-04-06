25a58c10a8f0fc4908f450c57d10ef596a62c344
Dominik Bauchner <dbauchner@gmail.com>
2022-04-06 07:01:49 +0200

    commit

---
+++ CPP/prednaska6_1/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+
+class Car {
+    class Motor{
+        int m_power;
+
+    public:
+        Motor (int power){
+            m_power = power;
+        }
+
+        int getPower(){
+            return m_power;
+        }
+
+    };
+
+    string m_color;
+    Motor* m_motor;
+
+public:
+    Car(string color, int motorPower){
+        m_color = color;
+        m_motor = new Motor(motorPower);
+    }
+
+    ~Car(){
+        delete m_motor;
+    }
+
+    void printInfo(){
+        cout << "Color: " << m_color << endl;
+        cout << "Engine Power: " << m_motor->getPower() << endl;
+    }
+
+
+
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
