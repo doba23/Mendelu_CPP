2998127ea5b73be60011bf1b1c9e2318c5cf3349
Dominik Bauchner <dbauchner@gmail.com>
2022-04-06 20:30:49 +0200

    commit

--- CPP/prednaska6_3/main.cpp
+++ CPP/prednaska6_3/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class NoveAuto{
     class Pneumatika{
         float m_tlak;
+    public:
         Pneumatika(float tlak){
             m_tlak = tlak;
         }
@@ -26,6 +27,13 @@ public:
         m_pneumatiky.at(3) = new Pneumatika(tlakPneumatik.at(3));
     }
 
+    ~NoveAuto(){
+        delete m_pneumatiky.at(0);
+        delete m_pneumatiky.at(1);
+        delete m_pneumatiky.at(2);
+        delete m_pneumatiky.at(3);
+    }
+
     void setTlakPneumatik(int jakaPneumatika, float tlak){
         m_pneumatiky.at(jakaPneumatika)->setTlak(tlak);
     }
@@ -37,18 +45,11 @@ public:
 
 
 int main() {
-
-//    Auto* honda = new Auto({4.3,4.2,4.1,4.4});
-//    honda->setTlakPneumatik(0, 4.5);
-//    cout << "Tlak: " << honda->getTlakPneumatik(0) << endl;
-
     NoveAuto* honda = new NoveAuto({4.3,4.2,4.1,4.4});
+    cout << "Tlak: " << honda->getTlakPneumatik(0) << endl;
     honda->setTlakPneumatik(0, 4.5);
     cout << "Tlak: " << honda->getTlakPneumatik(0) << endl;
 
-//    array<int, 5> myArray = {50,10,20,30,40};
-//    cout << "velikost: " << myArray.size() << endl;
-//    cout << "hodnoty arraye: " << myArray.at(0) << endl;
     delete honda;
     return 0;
 }
